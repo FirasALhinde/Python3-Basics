7727b817ede5cfc746375ac3de568c74a8df52a4
FirasALhinde <firasalhinde98@gmail.com>
2022-04-05 00:56:09 +0300

    add python condations

---
+++ 01-conditions.py
@@ -0,0 +1,46 @@
+#simple condision
+
+x = 5
+if x == 3:
+    print("x==3")    
+else:
+    print(x)
+
+
+#if elif else
+
+x = 10
+if x == 3:
+    print("x==3")
+elif x<2:
+    print("x>2")
+elif x<6:
+    print("x<6")
+    
+else:
+    print(x)
+
+
+x=11
+
+if x <10:
+    print("x<10")
+    if x < 5:
+        print("x<5")
+    else:
+        print("x>5")
+else:
+    print("x>10")
+    
+
+#if with bolean opeators
+username = "firas"
+password = 1234578
+
+if username=="firas" and password ==12345:
+    print("welcome")
+
+if username=="firas" or password ==12345:
+    print("welcome2")
+    
+    
